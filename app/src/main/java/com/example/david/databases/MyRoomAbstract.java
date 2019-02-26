62470acaa17214167fae44077b97e0fbc2e3c6c8
daalqui <davidaleq@gmail.com>
2019-02-26 16:31:23 +0100

    Ejercicio 3.5 - crear room abstract class + clase

---
+++ app/src/main/java/com/example/david/databases/MyRoomAbstract.java
@@ -0,0 +1,22 @@
+package com.example.david.databases;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.david.pojos.Quotation;
+
+@Database(version = 1, entities = {Quotation.class})
+public abstract class MyRoomAbstract extends RoomDatabase {
+
+    private static MyRoomAbstract myRoomAbstract;
+
+    public synchronized static MyRoomAbstract getInstance(Context context){
+        if(myRoomAbstract == null){
+            myRoomAbstract = Room.databaseBuilder(context, MyRoomAbstract.class, "quotation_table").
+                    allowMainThreadQueries().build();
+        }
+        return myRoomAbstract;
+    }
+}
